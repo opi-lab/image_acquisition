7c35f4b1c8cfbfcf8d07a47f3c101571ea06ee56
Kevin Mendoza <54753737+kevin10mv@users.noreply.github.com>
2020-02-26 16:40:45 -0500

    Update starter_image_acquisition_camera_gige.m

--- starter_image_acquisition_camera_gige.m
+++ starter_image_acquisition_camera_gige.m
@@ -1,5 +1,8 @@
 % files to add to the path: CalculateFrameRate.m | CalculatePacketDelay.m
-clear all; close all; clc; global camera; % close, delete and clean the executed application.
+clear all; 
+close all; 
+clc; global camera; % close, delete and clean the executed application.
+
 adaptor = 'gige'; device = 1; format = 'mono8'; % settings parameters of the acquisition device.
 camera = videoinput(adaptor,device,format); % create object instance of monochrome GigE camera.
 properties = getselectedsource(camera); % extracting camera properties.
@@ -8,9 +11,6 @@ resolution = get(camera,'videoresolution'); % obtain resolution of the acquisiti
 bands = get(camera,'numberofbands'); % it's number of color bands in the data to be acquired.
 properties.ExposureTimeRaw = 22000; % light penetrates the camera and reaches the digital sensor.
 properties.PacketSize = 1500; % real data that is transferred from the camera to the equipment.
-frames_to_acquire = 5; frames_per_second = CalculateFrameRate(camera, frames_to_acquire);
-delay = CalculatePacketDelay(camera, frames_per_second); % optimize packet delivery time.
-properties.PacketDelay = delay; % difference between time taken by the transmitted packets from source.
 height = image(zeros(resolution(2),resolution(1),bands)); % estimate height that you must have preview.
 window = figure(1); preview(camera,height); % new figure inserted to store the camera preview data.
 set(window,'keypressfcn',@keyboardpress); % link the control method from keyboard to current window.

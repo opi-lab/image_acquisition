95200fe53d1aa8c8c4407541a80650ca4faef238
Kevin Mendoza <54753737+kevin10mv@users.noreply.github.com>
2020-02-27 09:13:38 -0500

    Update image_acquisition_camera_usb3.m

--- image_acquisition_camera_usb3.m
+++ image_acquisition_camera_usb3.m
@@ -1,6 +1,7 @@
 clear all;
 close all; 
-clc; global camera; % close, delete and clean the executed application.
+clc;
+global camera; % close, delete and clean the executed application.
 adaptor = 'gentl'; device = 1; format = 'mono8'; % settings parameters of the acquisition device.
 camera = videoinput(adaptor,device,format); % create object instance of monochrome usb 3.0 camera.
 properties = getselectedsource(camera); % extracting camera properties.
